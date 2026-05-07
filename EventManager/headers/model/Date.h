13399bf066e56f7f9a560bbb338952e084a596ce
JjPEReirA775 <1240715@isep.ipp.pt>
2026-05-07 14:59:16 +0100

    Class Data

---
+++ EventManager/headers/model/Date.h
@@ -0,0 +1,25 @@
+// Date.h
+#ifndef DATE_H
+#define DATE_H
+
+#include <string>
+
+class Date{
+private:
+    int day;
+    int month; // 1- january, ... 12- December
+    int year;
+    bool isLeapYear(int year);
+    bool isValid(int day, int month, int year);
+public:
+    Date();
+    Date(int day, int month, int year);
+    Date(const Date & date);
+    void setDate(int day, int month, int year);
+    void getDate(int& day, int& month, int& year) const;
+    bool operator == (const Date& obj) const;
+    bool operator > (const Date& obj)const;
+    bool operator < (const Date& obj) const;
+};
+
+
