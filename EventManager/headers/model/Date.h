aa15615481073730118a72ef018d4fc0b68b6c1a
JjPEReirA775 <1240715@isep.ipp.pt>
2026-05-08 18:41:38 +0100

    Class Data(atualizaçao com hora e minuto)

--- EventManager/headers/model/Date.h
+++ EventManager/headers/model/Date.h
@@ -1,25 +1,27 @@
-// Date.h
-#ifndef DATE_H
-#define DATE_H
-
-#include <string>
+#ifndef HEADERS_MODEL_DATE_H_
+#define HEADERS_MODEL_DATE_H_
 
 class Date{
 private:
     int day;
     int month; // 1- january, ... 12- December
     int year;
+    int hour;
+    int minute;
     bool isLeapYear(int year);
-    bool isValid(int day, int month, int year);
+    bool isValid(int year, int month, int day,int hour,int minute);
 public:
     Date();
-    Date(int day, int month, int year);
+    Date(int year, int month, int day,int hour,int minute);
     Date(const Date & date);
-    void setDate(int day, int month, int year);
-    void getDate(int& day, int& month, int& year) const;
+
+    void setDate(int year, int month, int day,int hour,int minute );
+    void getDate(int &year, int &month, int &day,int &hour,int &minute) const;
     bool operator == (const Date& obj) const;
     bool operator > (const Date& obj)const;
     bool operator < (const Date& obj) const;
 };
 
+
+#endif /* HEADERS_MODEL_DATE_H_ */
 
--- EventManager/sources/model/Date.cpp
+++ EventManager/sources/model/Date.cpp
@@ -5,104 +5,144 @@ using namespace std;
 
 bool Date:: isLeapYear(int year){
 
+	if (year % 400 == 0)
+		return true;
+	/
+	if (year % 100 == 0)
+		return false;
 
-    if (year % 400 == 0)
-        return true;
-
-
-    if (year % 100 == 0)
-        return false;
-    // Else If a year is multiple of 4,
-    // then it is a leap year
-    if (year % 4 == 0)
-        return true;
-    return false;
+	if (year % 4 == 0)
+		return true;
+	return false;
 
 
 }
-bool Date::isValid(int day, int month, int year){
-    bool result = true;
-    if(day <= 0 || day > 31 || month <= 0 || month > 12){
-        result = false;
-    }else{
-        switch(month){
-            case 2:
-                if(isLeapYear(year) == true){
-                    if(day > 29){
-                        result = false;
-                    }
-                }else{
-                    if(day > 28){
-                        result = false;
-                    }
-                }
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                if(day > 30){
-                    result = false;
-                }
-                break;
-        }
-    }
-    return result;
+bool Date::isValid(int year, int moth, int day,int hour,int minute){
+	bool result = true;
+	if(day <= 0 || day > 31 || month <= 0 || month > 12){
+		result = false;
+	}else{
+		switch(month){
+		case 2:
+			if(isLeapYear(year) == true){
+				if(day > 29){
+					result = false;
+				}
+			}else{
+				if(day > 28){
+					result = false;
+				}
+			}
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			if(day > 30){
+				result = false;
+			}
+			break;
+		}
+	if (  minute < 0 && minute > 59 ) {
+		result = false;
+	}
+		if (  hour < 0 && hour > 23 ) {
+			result = false;
+		}
+	}
+	return result;
 
 }
 Date::Date(){
-    //default date
-    setDate(1,1, 1900);
+	//default date
+	setDate(1,1, 1900,0,0);
 }
-Date::Date(int day, int month, int year){
-    setDate(day,month, year);
+Date::Date(int year, int month, int day,int hour,int minute){
+	setDate(year,month,day,hour,minute);
 }
 Date::Date(const Date& date){
-    setDate(date.day, date.month, date.year);
+	setDate(date.year, date.month, date.day,date.hour,date.minute);
 }
-void Date::setDate(int day, int month, int year){
-    if(isValid(day, month, year)){
-        this->day = day;
-        this->month = month;
-        this->year = year;
-    }
-    else{
-        string msg =to_string(day)+ "/"+to_string(month)+"/"+to_string(year);
-        throw InvalidDataException(msg);
-    }
+void Date::setDate(int year, int month, int day,int hour, int minute){
+	if(isValid(year,month,day,hour,minute)){
+			this->day = day;
+			this->month = month;
+			this->year = year;
+			this->hour = hour;
+			this->minute = minute;
+		}
+		else{
+			string msg =to_string(day)+ "/"+to_string(month)+"/"+to_string(year);
+			throw InvalidDataException(msg);
+		}
 }
-void Date::getDate(int& day, int& month, int& year) const{
-    day = this->day;
-    month = this->month;
-    year = this->year;
+void Date::getDate(int& year, int& month, int& day,int& hour,int& minute) const{
+	day = this->day;
+	month = this->month;
+	year = this->year;
+	hour = this->hour;
+	minute = this->minute;
 }
 bool Date::operator == (const Date& obj) const{
-    if(this->day == obj.day && this->month == obj.month && this->year == obj.year){
-        return true;
-    }
-    return false;
+	if(this->day == obj.day && this->month == obj.month && this->year == obj.year && this->hour == obj.hour && this->minute == obj.minute){
+		return true;
+	}
+	return false;
 }
 bool Date::operator > (const Date& obj)const{
-    if(this->year > obj.year){
-        return true;
-    }
-    if(this->month > obj.month){
-        return true;
-    }
-    if(this->day > obj.day){
-        return true;
-    }
-    return false;
+	if(this->year > obj.year){
+			return true;
+		if(this->month > obj.month){
+			return true;
+			if(this->day > obj.day){
+				return true;
+				if(this->hour > obj.hour){
+					return true;
+				}
+				if(this->minute > obj.minute){
+					return true;
+				}
+	}
+	if(this->month > obj.month){
+		return true;
+		if(this->day > obj.day){
+			return true;
+			if(this->hour > obj.hour){
+				return true;
+			}
+			if(this->minute > obj.minute){
+				return true;
+			}
+	}
+	if(this->day > obj.day){
+		return true;
+		if(this->hour > obj.hour){
+			return true;
+		}
+		if(this->minute > obj.minute){
+			return true;
+		}
+	}
+	if(this->hour > obj.hour){
+		return true;
+		if(this->minute > obj.minute){
+			return true;
+		}
+	}
+	if(this->minute > obj.minute){
+		return true;
+	}
+	return false;
 }
 bool Date::operator < (const Date& obj) const{
-    if(*this == obj){
-        return false;
-    }
+	if(*this == obj){
+		return false;
+	}
 
-    if(*this > obj){
-        return false;
-    }
-    return true;
+	if(*this > obj){
+		return false;
+	}
+	return true;
 }
 //
 // Created by josej on 5/7/2026.
