a18196777af6f08675db9e34175f912ae87d6ce2
GabrielQMSilva <1241190@isep.ipp.pt>
2026-06-13 18:11:18 +0100

    .

--- EventManager/cmake-build-debug/CMakeFiles/EventManager.dir/sources/services/ClienteService.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/EventManager.dir/sources/services/ClienteService.cpp.obj: \
- C:\Users\Lord\ Foog\Documents\Aulas\ISEP\1o_ano\FSOFT\VirtualRepository\FSOFT2026_1DC_3\FSOFT2026_1DC_3\EventManager\sources\services\ClienteService.cpp
--- EventManager/headers/controllers/Controller.h
+++ EventManager/headers/controllers/Controller.h
@@ -5,4 +5,23 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include "View.h"
+#include "ClienteService.h"
+#include "SessaoService.h"
+
+class Controller {
+public:
+    enum class Role { UTILIZADOR, CLIENTE, ORGANIZADOR };
+
+    void run();
+
+private:
+    View view;
+    ClienteService *clienteService;
+
+    void handleClienteLogin();
+    void handleOrganizadorLogin();
+    void handleClienteRegistration();
+};
+
 #endif //CONTROLLER_H
